5573bedd5447b9c19fe4a0c611d200e447114c1e
ElleBerry <48695394+ElleBerry@users.noreply.github.com>
2019-05-15 10:09:34 -0500

    grades into letter grades

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,19 +1,22 @@
 import java.util.Scanner;
 public class ControlFlowExercises {
     public static void main(String[] args) {
-        System.out.println("Please enter an integer:");
-        Scanner sc = new Scanner(System.in).useDelimiter("\n");
-        int userInput = sc.nextInt();
+        System.out.println("Please enter the students grade.");
+        Scanner sc = new Scanner(System.in);
+        int grade = sc.nextInt();
         System.out.println("Do you want to continue?");
         String answer = sc.next();
-        System.out.println("Here is your table:");
-        System.out.println("Number | squared | cubed");
-        System.out.println("------ | ------- | ------");
         if(answer.equalsIgnoreCase("yes")){
-            for(int i = 1; i<=userInput; i++){
-                int squared = i*i;
-                int cubed = i*i*i;
-                System.out.printf("%-7d|%-9d|%-6d%n",i ,squared, cubed);
+            if(grade >= 88){
+                System.out.println("You got an A!");
+            }else if(grade >= 80 && grade <=87){
+                System.out.println("You got a B!");
+            }else if(grade>= 67 && grade <= 79){
+                System.out.println("You got a C!");
+            }else if(grade>=60 && grade <= 66){
+                System.out.println("You got a D!");
+            }else{
+                System.out.println("You got an F!");
             }
         }
     }
