fae3384c5baad60a68c92d000ce25a140783758e
ElleBerry <48695394+ElleBerry@users.noreply.github.com>
2019-05-15 09:41:27 -0500

    FIzzBuzz

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,9 +1,15 @@
 public class ControlFlowExercises {
     public static void main(String[] args) {
-
-        for(int i = 2; i<=10000;){
-                System.out.println(i*i);
-                i++;
+        for(int i = 1; i <= 100; i++){
+            if(i%15 ==0){
+                System.out.println("FizzBUzz");
+            }else if(i%5==0){
+                System.out.println("Buzz");
+            }else if(i%3==0){
+                System.out.println("Fizz");
+            }else{
+                System.out.println(i);
+            }
         }
     }
 }
