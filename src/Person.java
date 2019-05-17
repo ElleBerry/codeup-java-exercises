87f32bcbd9518b71dc4b9f2fc889d2bc6e3ac108
ElleBerry <48695394+ElleBerry@users.noreply.github.com>
2019-05-17 14:48:52 -0500

    I know more than I thought i did. also the fist commit was wrong

--- src/Person.java
+++ src/Person.java
@@ -1,16 +1,22 @@
 import java.util.Scanner;
 public class Person {
+
     static Scanner sc = new Scanner(System.in);
-    private static String name = "Elle";
+
+    private static String name;
+
+    public Person(String name){
+       this.name = name;
+    }
 
     public static String getName(){
-        System.out.println("Please enter a name:");
-        name = sc.nextLine();
         return name;
     }
 
     public static void setName(String name){
-
+        System.out.println("Please enter a name:");
+        name = sc.nextLine();
+       Person.name=name;
     }
 
     public static void sayHello(){
